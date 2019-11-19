ff3c545f6f76a2d6f753acd4e209e21c463a95f6
projectbackuprahul <projectbackuprahul@gmail.com>
2019-11-19 14:46:36 +0530

    modified in client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
-const express = require ('express');
+const express = require('express');
 const socketio = require('socket.io');
-const http = require ('http');
+const http = require('http');
+
+//@ Configuring os for network interfce .
+var os = require('os');
+var ifaces = os.networkInterfaces();
+//@ Configuring os for network interfce 
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,17 +17,40 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 // @ Implementing socket io .
-io.on('connection', (socket)=>{
-      console.log ("we have a new connnection ");
+io.on('connection', (socket) => {
+    console.log("we have a new connnection ");
 
-      socket.on('disconnect' , ()=>{
-          console.log ('User had Left')
-      })
+    socket.on('disconnect', () => {
+        console.log('User had Left')
+    })
 });
 
 // @ Using the Middlewares
 app.use(router);
 
-server.listen(PORT , () => {
-     console.log (`Server has started on port ${PORT}`);
+server.listen(PORT, () => {
+   genURL();
 })
+
+  //  @ Function generate local ip address 
+  
+    const genURL = () => {
+        Object.keys(ifaces).forEach((ifname) => {
+            var alias = 0;
+
+            ifaces[ifname].forEach((iface) => {
+                if ('IPv4' !== iface.family || iface.internal !== false) {
+
+                    return;
+                }
+
+                if (alias >= 1) {
+                   
+                } else {
+                    var url = 'http://' + iface.address + ':' + PORT + '/'
+                    console.log(url);
+                }
+                ++alias;
+            });
+        });
+    }
